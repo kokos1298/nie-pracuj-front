b3c8e2590e43114df0cfd35818dd63763e83c034
kamil <kamila1207@wp.pl>
2023-02-22 21:28:49 +0100

    initial

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import CollapsibleTable from "../components/CollapsibleTable";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <CollapsibleTable />
+        </>
+    );
+}
+
+export default App;
